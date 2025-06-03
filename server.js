169ce42f95449c1b2ebdbdd793b5f3871d5dcdac
unknown <mohseensk5@gmail.com>
2025-06-04 00:24:40 +0530

    render database chamge internal

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./app');
 const PORT = process.env.DB_PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server runninge on http://localhost:${PORT}`);
 })
