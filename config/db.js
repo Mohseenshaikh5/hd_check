84c52c44f9c03a337430f0ba22a53bd01e2abd6d
unknown <mohseensk5@gmail.com>
2025-06-04 01:35:02 +0530

    add ssl

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+// const { Pool } = require('pg');
+// require('dotenv').config();
 
 // const pool = new Pool({
 //   user: process.env.DB_USER,
@@ -12,11 +12,14 @@ require('dotenv').config();
 //   }
 // });
 
+const { Pool } = require('pg');
+require('dotenv').config();
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Required for Render
-  }
+    rejectUnauthorized: false, // Required for Render's self-signed certificate
+  },
 });
 
 pool.connect()
